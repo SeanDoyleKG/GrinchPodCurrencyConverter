59f9ff94c3ad7da1bf43388e6a70db79f4b65534
Marcello Borrani <marcelloborrani@kubrickgroup.com>
2025-11-05 14:43:48 +0000

    black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,10 +3,8 @@ from unittest.mock import patch
 from src.cli import command_line_arguments_for_currency_conversion
 
 
-
-@patch('sys.argv', ['cli.py', '-b', 'USD' '-t', 'GBP', '-a', '100.00', 'm'])
+@patch("sys.argv", ["cli.py", "-b", "USD" "-t", "GBP", "-a", "100.00", "m"])
 def test_command_line_argument_for_currency_conversion():
     x = command_line_arguments_for_currency_conversion()
 
-    assert x == ('USD', 'GBP', 100.00, True)
-    
+    assert x == ("USD", "GBP", 100.00, True)
