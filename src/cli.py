0ec2c9661dc82560cc0b77f5604fcd6788478404
Marcello Borrani <marcelloborrani@kubrickgroup.com>
2025-11-05 14:45:27 +0000

    i got an 8.85/10 for pylint and a cake for black

--- src/cli.py
+++ src/cli.py
@@ -1,22 +1,19 @@
 """A script to import argparse module."""
+
 import argparse
 
 # Creating an argument parser
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--base', '-b',
-                    help = 'base currency')
+parser.add_argument("--base", "-b", help="base currency")
 
-parser.add_argument('--target', '-t',
-                    help = 'target currency')
+parser.add_argument("--target", "-t", help="target currency")
 
-parser.add_argument('--amount', '-a',
-                    type = float,
-                    help = 'amount to convert')
+parser.add_argument("--amount", "-a", type=float, help="amount to convert")
 
-parser.add_argument('--mock', '-m',
-                    help = 'use mock data for testing',
-                    action = 'store_true')
+parser.add_argument(
+    "--mock", "-m", help="use mock data for testing", action="store_true"
+)
 
 # Parse arguments
 args = parser.parse_args()
@@ -28,13 +25,16 @@ amount = args.amount
 mock = args.mock
 
 
-print(f"{base_currency} will be converted to {target_currency} for the amount of {amount} {base_currency}.")# This should print out: USD is converted to EUR for the amount of 100 USD.
+print(
+    f"{base_currency} will be converted to {target_currency} for the amount of {amount} {base_currency}."
+)  # This should print out: USD is converted to EUR for the amount of 100 USD.
 print(type(amount))
 print(type(base_currency))
 print(type(target_currency))
 
 
 # TODO: log when people pass in the wrong arguments for amount (e.g., a string instead of a number)
+
 
 def command_line_arguments_for_currency_conversion():
     """Get currency conversion command line arguments from user.
@@ -44,5 +44,3 @@ def command_line_arguments_for_currency_conversion():
 
     """
     return base_currency, target_currency, amount, mock
-
-
